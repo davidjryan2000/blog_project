3213372f925ec2b3ead50b0753aee2ef74e9a938
David Ryan <davidjryan2000@gmail.com>
2017-07-03 14:58:25 +0100

    stopped tracking env.sh

--- env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo Configuring Environment Variables
-export DATABASE_URL="postgres://vsoirlfaxqprnu:33619835254cf2e343987e0a5c97ae78c3fd9af343c49c0b039ce5404a8633a9@ec2-54-217-212-8.eu-west-1.compute.amazonaws.com:5432/d9lhkg5upafuvs"
-export AWS_STORAGE_BUCKET_NAME=ryan-david-blog-prj
-export AWS_ACCESS_KEY_ID=AKIAJ6PUP4TEZVCFJHLQ
-export AWS_SECRET_ACCESS_KEY=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
-export SECRET_KEY='m^g8$wo_i29+40)&6cyq5*l5zna3(vs!!7#!1t20xp-(94^m8n'
-echo Done
